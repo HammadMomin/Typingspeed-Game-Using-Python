eb392caa4383e0897d76b40be4227eb43b5f9269
HammdMomin <mohdhammad.momin@gmail.com>
2022-07-08 01:52:04 +0530

    Initial Commit

---
+++ Age limit.py
@@ -0,0 +1,10 @@
+print("Age Limit Program")
+min_age=25
+max_age=50
+curr_age=(int(input("Enter Chefs current Age : ")))
+if (curr_age>=min_age):
+    print("Chef is Eligible for the Exam")
+#elif (curr_age<max_age):
+ #   print("Chef is not Eligible For the Exam")
+else:
+    print("Chef is Not Eligible for the Exam")
